39e65d63e96a19315e0db8e6b938bdffaaa9c428
Rakibul Alam <limonrakib05690@gmail.com>
2019-05-02 01:06:06 +0600

    Working with Title Component

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="container">
+            <h1 className="display-2 text-center">
+               Welcome to the React Js
+            </h1>     
+           
+        </div>
+    </div>
+  );
+}
+
+export default App;
